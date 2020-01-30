4ad86f1c0369a81024f3b2cb503a751f11f1521b
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-30 10:45:37 +0000

    Add Requirements
    Signed-off-by: Stephen Jackson <jjackson19862017@gmail.com>

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def hello():
 
     if __name__ == '__main__':
         app.run(host=os.environ.get('IP','0.0.0.0'),
-            port=int(os.environ.get('PORT','8888')),
+            port=os.environ.get('PORT','8888'),
             debug=True)
