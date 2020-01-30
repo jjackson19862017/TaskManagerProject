9c77ea50243db4e2c1f8f81956468b9bfd753151
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-30 10:56:37 +0000

    updated app.py
    Signed-off-by: Stephen Jackson <jjackson19862017@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World ...again'
 
-    if __name__ == '__main__':
-        app.run(host=os.environ.get('IP','0.0.0.0'),
-            port=os.environ.get('PORT','8888'),
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
